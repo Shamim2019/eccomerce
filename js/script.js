62b5bfab0e51fb6efd338b4ab4181ee096693fe5
Shamim2019 <shamimmaloba@gmail.com>
2022-03-27 20:32:26 +0300

    made functions

--- js/script.js
+++ js/script.js
@@ -13,3 +13,22 @@ if (close) {
          nav.classList.remove('active')
     })
 }
+
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
+
+smallimg [0].onclick = function(){
+    MainImg.src = smallimg[0].src;
+}
+smallimg [1].onclick = function(){
+    MainImg.src = smallimg[1].src;
+}
+smallimg [2].onclick = function(){
+    MainImg.src = smallimg[2].src;
+}
+smallimg [3].onclick = function(){
+    MainImg.src = smallimg[3].src;
+}
+smallimg [4].onclick = function(){
+    MainImg.src=smallimg[4].src;
+}
